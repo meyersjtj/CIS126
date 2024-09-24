0db6327ae3a1602cea7af21b8afdf44227315b5d
meyersjtj <49164975+meyersjtj@users.noreply.github.com>
2024-09-24 15:30:19 -0700

    Add files via upload

---
+++ Week 1/script.js
@@ -0,0 +1,5 @@
+let firstName = 'Josh';
+let lastName = 'Meyers';
+let yearsOfStudy = 'less than a year';
+let goal = 'earn a CS degree and transfer to a 4 year university.';
+let statement = document.write("Hello, my name is ", firstName, " ", lastName, ", and I have been at Pierce for ", yearsOfStudy, " and my goal is to ", goal);

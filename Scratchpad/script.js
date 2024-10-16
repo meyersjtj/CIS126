a6bd5878f7226ccb8213a82a83f278feb4d8b411
Josh Meyers <meyers.josh@gmail.com>
2024-10-16 13:24:01 -0700

    Week 4

--- Scratchpad/script.js
+++ Scratchpad/script.js
@@ -1,32 +1,35 @@
-let prize = prompt("Select a number between 1 and 10: ");
-prize = Number(prize);
-output = "My selection: ";
-switch(prize){
-    case 0:
-        console.log("A stuffed bunny.");
-        break;
-    case 1:
-        console.log("A stuffed dog.");
-        break;
-    case 2:
-        console.log("A stuffed cat.");
-        break;
-    case 3:
-        console.log("A stuffed dolphin");
-        break;
-    case 4:
-        console.log("A stuffed wolf");
-        break;
-    case 5:
-        console.log("Prize 5.");
-        break;
-    case 6:
-        console.log("Prize 6.");
-        break;
-    case 7:
-        console.log("Prize 7.");
-        break;
-    default:
-        console.log("What ever you can think of.");
+const validatePassword = (password) => {
+    let pass_length = 0;
+    let upper_case_count = 0;
+    let lower_case_count = 0;
+    let meets_reqs = false;
+    lower_case_alpha = "abcdefghijklmnopqrstuvwxyz".split("");
+    console.log(lower_case_alpha);
+    upper_case_alpha = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+    console.log(upper_case_alpha);
+    
+    const pass_array = password.split('');
+    for (let n in pass_array){
+        if (lower_case_alpha.includes(pass_array[n])){
+            lower_case_count += 1;
+        }
+        else if (upper_case_alpha.includes(pass_array[n])){
+            upper_case_count += 1;
+        }
+    }
+    console.log("Lower case characters: ", lower_case_count);
+    console.log("Upper case characters: ", upper_case_count);
+    console.log("Password length: ", pass_array.length);
+
+    if (pass_array.length >= 8 && upper_case_count > 0 && lower_case_count > 0) {
+        meets_reqs = true
+        document.write("Meets password requirements");
+    }
+    else {
+        document.write("Does NOT meet password requirements");
+    }
+    return meets_reqs;
 }
-console.log(output);
+let password = "HeeeelllLLoooooo";
+let pass_reqs_met = validatePassword(password);
+console.log(pass_reqs_met);

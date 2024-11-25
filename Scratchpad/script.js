1b6ad8341056e3784f7e47fbc5b75609b3e01df3
Josh Meyers <meyers.josh@gmail.com>
2024-11-24 20:27:51 -0800

    Week 9 - Drawing a Picture

--- Scratchpad/script.js
+++ Scratchpad/script.js
@@ -1,12 +1,30 @@
-const promise = new Promise((resolve, reject) => {
-    reject('Error');
-})
-    .catch((error) => console.log(error)) // Catch the rejection
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
 
-console.log("Start counting");
-Promise.resolve("One")
-    .then(value => console.log(value))
-    .then(() => Promise.resolve("Two"))
-    .then(value => console.log(value))
-    .then(() => Promise.resolve("Three"))
-    .then(value => console.log(value));
+var window_height = window.innerHeight;
+var window_width = window.innerWidth;
+
+canvas.width = window_width;
+canvas.height = window_height;
+
+canvas.style.backgroundColor = '#ff8';
+
+class Circle {
+    constructor(xpos, ypos, radius, color) {
+        this.xpos = xpos;
+        this.ypos = ypos;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw(context) {
+        context.beginPath();
+        context.arc(this.xpos, this.ypos, this.radius, 0, 2 * Math.PI, false);
+        context.fillStyle = this.color;
+        context.stroke();
+    }
+}
+
+let my_circle = new Circle(100, 100, 50, 'black');
+
+my_circle.draw(context);
--- Week 9/drawmeapicture.js
+++ Week 9/drawmeapicture.js
@@ -1,43 +1,88 @@
 let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext("2d");
+let context = canvas.getContext("2d");
 
 canvas.width = 500;
 canvas.height = 500;
 
-function draw() {
-    // Draw lines in from each corner
-    ctx.moveTo(0, 0);
-    ctx.lineTo(50, 50);
-    ctx.stroke();
-    ctx.moveTo(500, 0);
-    ctx.lineTo(450, 50);
-    ctx.stroke();
-    ctx.moveTo(0, 500);
-    ctx.lineTo(50, 450);
-    ctx.stroke();
-    ctx.moveTo(500, 500);
-    ctx.lineTo(450, 450);
-    ctx.stroke();
+class Circle {
+  constructor(xpos, ypos, radius, color) {
+    this.xpos = xpos;
+    this.ypos = ypos;
+    this.radius = radius;
+    this.color = color;
+  }
 
-    // Draw a square
-    ctx.fillStyle = "red";
-    ctx.fillRect(50, 50, 400, 400);
-
-    // Draw a circle
-    ctx.fillStyle = "green";
-    ctx.beginPath();
-    ctx.arc(250, 250, 200, 0, 2 * Math.PI);
-    ctx.fill();
-
-    // Draw a triangle
-    ctx.fillStyle = "blue";
-    ctx.beginPath();
-    ctx.moveTo(250, 50);
-    ctx.lineTo(450, 450);
-    ctx.lineTo(50, 450);
-    ctx.fill();
-    
-
+  draw(context) {
+    context.beginPath();
+    context.arc(this.xpos, this.ypos, this.radius, 0, 2 * Math.PI, false);
+    context.stroke();
+    context.closePath();
+  }
 }
 
-window.addEventListener("load", draw);
+class Rectangle {
+  constructor(xpos, ypos, width, height, color) {
+    this.xpos = xpos;
+    this.ypos = ypos;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  draw(context) {
+    context.beginPath();
+    context.rect(this.xpos, this.ypos, this.width, this.height);
+    context.fillStyle = this.color;
+    context.fill();
+  }
+}
+
+class Line {
+  constructor(xpos, ypos, xpos2, ypos2, color) {
+    this.xpos = xpos;
+    this.ypos = ypos;
+    this.xpos2 = xpos2;
+    this.ypos2 = ypos2;
+    this.color = color;
+  }
+
+  draw(context) {
+    context.beginPath();
+    context.moveTo(this.xpos, this.ypos);
+    context.lineTo(this.xpos2, this.ypos2);
+    context.strokeStyle = this.color;
+    context.stroke();
+  }
+}
+
+function clearCanvas() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+document.getElementById("clear").addEventListener("click", clearCanvas);
+
+let my_circle = new Circle(100, 100, 50, "black");
+let my_line = new Line(0, 0, 500, 500, "red");
+
+let rectX = 0;
+let rectY = 0;
+
+function animateRectangle() {
+    clearCanvas();
+    let my_rectangle = new Rectangle(rectX, rectY, 50, 50, 'red');
+    my_rectangle.draw(context);
+    rectX += 10;
+    rectY += 10;
+    if (rectX < 500 && rectY < 500) {
+        requestAnimationFrame(animateRectangle);
+    }
+    else {
+        clearCanvas();
+        my_circle.draw(context);
+        my_line.draw(context);
+    }
+}
+my_circle.draw(context);
+my_line.draw(context);
+requestAnimationFrame(animateRectangle);
+
